cf09c98cb8a255b22bbfbe3d16030c1575b5bf3c
Mostafa AbdEl Fatah <mostafaabdelfatah@Mostafas-Mac.local>
2018-10-19 14:06:50 -0700

    first commit

--- GestureRecognizerDemo/ViewController.swift
+++ GestureRecognizerDemo/ViewController.swift
@@ -10,15 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var backgroundView: UIView!{
+        didSet{
+            /*let swipe = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.backgroundViewSwiped))
+            swipe.direction = [.left,.right]*/
+            let leftSwipe = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.backgroundViewLeftSwiped))
+            leftSwipe.direction = [.left]
+            let rightSwipe = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.backgroundViewRightSwiped))
+            rightSwipe.direction = [.right]
+            backgroundView.addGestureRecognizer(leftSwipe)
+            backgroundView.addGestureRecognizer(rightSwipe)
+            let pinch = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.backgroundViewPinch(_:)))
+            backgroundView.addGestureRecognizer(pinch)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func backgroundViewPinch(_ sender:UIPinchGestureRecognizer){
+        print("\(sender.scale)")
+        let num = Int(sender.scale) + 10
+        numberLabel.font = numberLabel.font.withSize(CGFloat( num * 15 ) )
     }
+    
+    @objc func backgroundViewLeftSwiped(_ sender:UISwipeGestureRecognizer){
+        var num = Int( numberLabel.text! )!
+        num -= 1
+        numberLabel.text = "\(num)"
+    }
+    
+    @objc func backgroundViewRightSwiped(_ sender:UISwipeGestureRecognizer){
+        var num = Int( numberLabel.text! )!
+        num += 1
+        numberLabel.text = "\(num)"
+    }
+    
+    @IBAction func backgroundViewTaped(_ sender: UITapGestureRecognizer) {
+        backgroundView.backgroundColor = UIColor.randomColor()
+    }
+    
 
 
 }
